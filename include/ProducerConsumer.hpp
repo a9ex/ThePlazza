d2518fb93f328b57a3aa858272b0eecc50f92ec9
DURAND Malo <malo.durand@epitech.eu>
2024-05-09 02:39:36 +0200

    fix: untracked header file

---
+++ include/ProducerConsumer.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** Concurrent Programming
+** File description:
+** ProducerConsumer.hpp
+*/
+
+#ifndef __PRODUCER_CONSUMER_HPP_
+    #define __PRODUCER_CONSUMER_HPP_
+
+void *Consumer(void *arg);
+void *Producer(void *arg);
+
+#endif /* !__PRODUCER_CONSUMER_HPP_ */
