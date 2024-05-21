f685fbe8f5e3151a3199a2b6ad097dd0c84cfc56
A1ex <a1ex@a1ex.fr>
2024-05-21 02:19:15 +0200

    feat: document header files

--- include/Pizza.hpp
+++ include/Pizza.hpp
@@ -8,7 +8,10 @@
 #include <string>
 #include <vector>
 namespace plazza {
-    // Do not modify the following enums, required in the subject
+    /**
+     * @brief Represents the type of a pizza (do not modify the values)
+     * Required by the subject
+     */
     enum PizzaType {
         Regina = 1 ,
         Margarita = 2 ,
@@ -16,6 +19,10 @@ namespace plazza {
         Fantasia = 8
     };
 
+    /**
+     * @brief Represents the size of a pizza (do not modify the values)
+     *
+     */
     enum PizzaSize {
         S = 1,
         M = 2,
@@ -24,7 +31,10 @@ namespace plazza {
         XXL = 16
     };
 
-    // Can be modified
+    /**
+     * @brief Represents the ingredients of a pizza
+     *
+     */
     enum PizzaIngredient {
         Dough = 1,
         Tomato,
@@ -43,14 +53,54 @@ namespace plazza {
             Pizza() = default;
             ~Pizza() = default;
 
+            /**
+             * @brief Get the cooking time of the pizza
+             *
+             * @return double the cooking time in seconds
+             */
             double getCookingTime() const;
+            /**
+             * @brief Get the Type of the pizza
+             *
+             * @return PizzaType the type of the pizza
+             */
             PizzaType getType() const;
+            /**
+             * @brief Get the display name of the pizza
+             *
+             * @return std::string the name of the pizza
+             */
             std::string getName() const;
+            /**
+             * @brief Get the Ingredients of the pizza
+             *
+             * @return std::vector<PizzaIngredient> a vector of ingredients
+             */
             std::vector<PizzaIngredient> getIngredients() const;
 
+            /**
+             * @brief Set the cooking time of the pizza
+             *
+             * @param time the cooking time in seconds
+             */
             void setCookingTime(double time);
+            /**
+             * @brief Set the type of the pizza (used internally)
+             *
+             * @param type the type of the pizza
+             */
             void setType(PizzaType type);
+            /**
+             * @brief Set the display name of the pizza (in console)
+             *
+             * @param name the name of the pizza
+             */
             void setName(const std::string &name);
+            /**
+             * @brief Set the ingredients of the pizza
+             *
+             * @param ingredients a vector of ingredients
+             */
             void setIngredients(const std::vector<PizzaIngredient> &ingredients);
         protected:
         private:
--- include/PizzaBuilder.hpp
+++ include/PizzaBuilder.hpp
@@ -12,16 +12,46 @@
 #pragma once
 
 namespace plazza {
+    // Represents a pizza builder, used to create easily a pizza
     class PizzaBuilder {
         public:
             PizzaBuilder() = default;
             ~PizzaBuilder() = default;
 
+            /**
+             * @brief Set the type of the pizza
+             *
+             * @param type the type of the pizza
+             * @return PizzaBuilder& the builder (to chain calls)
+             */
             PizzaBuilder &setPizzaType(PizzaType type);
+            /**
+             * @brief Set the name of the pizza
+             *
+             * @param name the name of the pizza
+             * @return PizzaBuilder& the builder (to chain calls)
+             */
             PizzaBuilder &setPizzaName(const std::string &name);
+            /**
+             * @brief Set the cooking time of the pizza
+             *
+             * @param time the cooking time in seconds
+             * @return PizzaBuilder& the builder (to chain calls)
+             */
             PizzaBuilder &setCookingTime(double time);
+            /**
+             * @brief Set the ingredients of the pizza
+             *
+             * @param ingredients a vector of ingredients
+             * @return PizzaBuilder& the builder (to chain calls)
+             */
             PizzaBuilder &setIngredients(const std::vector<PizzaIngredient> &ingredients);
 
+            /**
+             * @brief Build the pizza
+             *
+             * @return Pizza the pizza
+             */
             Pizza build();
         protected:
         private:
