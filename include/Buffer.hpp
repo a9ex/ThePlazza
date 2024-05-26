125223af899c5e15d791c273979036ee7bde775b
Adam CAVILLON <contact@codinbox.fr>
2024-05-26 18:08:07 +0200

    feat: move ByteBuf logic

--- include/Buffer.hpp
+++ include/Buffer.hpp
@@ -21,103 +21,22 @@ namespace buffer {
         std::size_t size() const { return this->_buffer.size(); }
         std::vector<char> getBuffer() const { return this->_buffer; }
 
-        char readChar() {
-            char value = this->_buffer[0];
-            this->_buffer.erase(this->_buffer.begin());
-            return value;
-        }
-
-        int readInt() {
-            int value;
-            std::memcpy(&value, this->_buffer.data(), sizeof(int));
-            this->_buffer.erase(this->_buffer.begin(), this->_buffer.begin() + sizeof(int));
-            return value;
-        }
-
-        long readLong() {
-            long value;
-            std::memcpy(&value, this->_buffer.data(), sizeof(long));
-            this->_buffer.erase(this->_buffer.begin(), this->_buffer.begin() + sizeof(long));
-            return value;
-        }
-
-        float readFloat() {
-            float value;
-            std::memcpy(&value, this->_buffer.data(), sizeof(float));
-            this->_buffer.erase(this->_buffer.begin(), this->_buffer.begin() + sizeof(float));
-            return value;
-        }
-
-        double readDouble() {
-            double value;
-            std::memcpy(&value, this->_buffer.data(), sizeof(double));
-            this->_buffer.erase(this->_buffer.begin(), this->_buffer.begin() + sizeof(double));
-            return value;
-        }
-
-        std::string readString() {
-            std::string value;
-            std::size_t size = this->readInt();
-            for (std::size_t i = 0; i < size; i++)
-                value.push_back(this->readChar());
-            return value;
-        }
-
-        unsigned long readUnsignedLong() {
-            unsigned long value;
-            std::memcpy(&value, this->_buffer.data(), sizeof(unsigned long));
-            this->_buffer.erase(this->_buffer.begin(), this->_buffer.begin() + sizeof(unsigned long));
-            return value;
-        }
-
-        void writeChar(char c) {
-            this->_buffer.push_back(c);
-        }
-
-        void writeInt(int i) {
-            std::vector<char> bytes(sizeof(int));
-            std::memcpy(bytes.data(), &i, sizeof(int));
-            this->_buffer.insert(this->_buffer.end(), bytes.begin(), bytes.end());
-        }
-
-        void writeLong(long l) {
-            std::vector<char> bytes(sizeof(long));
-            std::memcpy(bytes.data(), &l, sizeof(long));
-            this->_buffer.insert(this->_buffer.end(), bytes.begin(), bytes.end());
-        }
-
-        void writeFloat(float f) {
-            std::vector<char> bytes(sizeof(float));
-            std::memcpy(bytes.data(), &f, sizeof(float));
-            this->_buffer.insert(this->_buffer.end(), bytes.begin(), bytes.end());
-        }
-
-        void writeDouble(double d) {
-            std::vector<char> bytes(sizeof(double));
-            std::memcpy(bytes.data(), &d, sizeof(double));
-            this->_buffer.insert(this->_buffer.end(), bytes.begin(), bytes.end());
-        }
-
-        void writeBuffer(std::vector<char> buffer) {
-            for (auto &c : buffer)
-                this->_buffer.push_back(c);
-        }
-
-        void writeBuffer(ByteBuf buffer) {
-            this->writeBuffer(buffer.getBuffer());
-        }
-
-        void writeString(std::string str) {
-            this->writeInt(str.size());
-            for (auto &c : str)
-                this->writeChar(c);
-        }
-
-        void writeUnsignedLong(unsigned long l) {
-            std::vector<char> bytes(sizeof(unsigned long));
-            std::memcpy(bytes.data(), &l, sizeof(unsigned long));
-            this->_buffer.insert(this->_buffer.end(), bytes.begin(), bytes.end());
-        }
+        char readChar();
+        int readInt();
+        long readLong();
+        float readFloat();
+        double readDouble();
+        std::string readString();
+        unsigned long readUnsignedLong();
+        void writeChar(char c);
+        void writeInt(int i);
+        void writeLong(long l);
+        void writeFloat(float f);
+        void writeDouble(double d);
+        void writeBuffer(std::vector<char> buffer);
+        void writeBuffer(ByteBuf buffer);
+        void writeString(std::string str);
+        void writeUnsignedLong(unsigned long l);
     private:
         std::vector<char> _buffer;
     };
